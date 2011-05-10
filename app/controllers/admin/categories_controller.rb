9650d3d7f19a6efe4d3f99ca5d0c796f808d5d11
wxianfeng <wang.fl1429@gmail.com>
2011-05-11 00:49:25 +0800

    modify category CRUD , not used inherited_resources

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+  before_filter :find_category , :only=>[:show,:edit,:update,:destroy]
   
   def index
-    @categories = Category.all
+    @categories = Category.order("show_order DESC")
     @chart = Shopdls::OrgChart.new
     @chart.add_column('string', 'Name' )
     @chart.add_column('string', 'Manager')
@@ -22,6 +23,43 @@ class Admin::CategoriesController < ApplicationController
   
   def new
     @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to admin_category_path(@category)  , :notice=>"create success"
+    else
+      redirect_to :back , :notice=>"ERROR"
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @category.update_attributes(params[:category])
+      redirect_to admin_category_path(@category) , :notice => "Update Success"
+    else
+      redirect_to :back , :notice=>"ERROR"
+    end
+  end
+  
+  def destroy
+    if @category.destroy
+      redirect_to admin_categories_path , :notice=>"Delete Success"
+    else
+      redirect_to :back , :notice=>"ERROR"
+    end
+  end
+  
+  protected
+  
+  def find_category
+    @category = Category.find_by_id(params[:id])
   end
   
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :products
+  has_many :childrens , :foreign_key => "parent_id" , :class_name=>"Category"
+  belongs_to :parent , :foreign_key => "parent_id" ,  :class_name=>"Category"
   
-  validates :name , :presence => true
-  
-  def children
-    Category.where(:category_id=>self.id)
-  end
+  validates :name , :presence => true  
+  validates :show_order , :format => { :with => /\d+/ , :message => "must number" } ,:uniqueness => true
   
 end
