28862d60849abfcf373c016efb9ae966d6b6a74e
wxianfeng <wang.fl1429@gmail.com>
2011-05-10 13:11:49 +0800

    show case of category tree

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,23 @@
 class Admin::CategoriesController < ApplicationController
-  inherit_resources
+  
+  def index
+    @categories = Category.all
+    @chart = Shopdls::OrgChart.new
+    @chart.add_column('string', 'Name' )
+    @chart.add_column('string', 'Manager')
+    @chart.add_column('string', 'ToolTip')
+    @chart.add_rows( [
+        [ {:v => 'Mike', :f => 'Mike<div style="color:red; font-style:italic">President</div>' }, '' , 'The President' ],
+        [ {:v => 'Jim' , :f => 'Jim<div style="color:red; font-style:italic">Vice President<div>'}, 'Mike', 'VP' ],
+        [ 'Alice' , 'Mike', '' ],
+        [ 'Bob' , 'Jim' , 'Bob Sponge' ],
+        [ 'Carol' , 'Bob' , '' ]
+      ] )
 
-  protected
-
-  def collection
-    @categories ||= end_of_association_chain.page(params[:page])
+    options = { :allowHtml => true }
+    options.each_pair do | key, value |
+      @chart.send "#{key}=", value
+    end
   end
+  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :products
+  
+  def children
+    Category.where(:category_id=>self.id)
+  end
+  
 end
