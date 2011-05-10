58f53843f6f37762a3c51d8a16d92edee7ea1598
wxianfeng <wang.fl1429@gmail.com>
2011-05-10 11:44:26 +0800

    add google org chart lib , prepare for category show

---
+++ lib/shopdls.rb
@@ -0,0 +1,1 @@
+require File.expand_path("../shopdls/org_chart",__FILE__)
---
+++ lib/shopdls/org_chart.rb
@@ -0,0 +1,30 @@
+## 
+# SEE: https://github.com/winston/google_visualr/blob/master/lib/org_chart.rb
+
+require File.expand_path("../base_chart",__FILE__)
+
+module Shopdls
+
+  # http://code.google.com/apis/visualization/documentation/gallery/orgchart.html
+  class OrgChart < BaseChart
+
+    attr_accessor :element_id
+
+    # http://code.google.com/apis/visualization/documentation/gallery/orgchart.html#Configuration_Options
+    attr_accessor :allowCollapse
+    attr_accessor :allowHtml
+    attr_accessor :nodeClass
+    attr_accessor :selectedNodeClass
+    attr_accessor :size
+
+    def render (element_id)
+      options = Hash.new
+      options[:package]     = self.class.to_s.split('::').last
+      options[:element_id]  = element_id
+      options[:chart_style] = collect_parameters
+      super(options)
+    end
+
+  end
+
+end
