7671a661291e9a04324906e253a418ade68dbc3d
wxianfeng <wang.fl1429@gmail.com>
2011-05-19 01:36:18 +0800

    move custom requires to application.rb , not in config/initalizers

--- config/initializers/requires.rb
+++
@@ -1,5 +0,0 @@
-# Require recursively all files in the lib/patches
-# Dir[Rails.root.join('lib/patches/**/*.rb')].each {|f| require f}
-
-# Require only files placed in the lib folder
-Dir[Rails.root.join('lib/*.rb')].each {|f| require f}
