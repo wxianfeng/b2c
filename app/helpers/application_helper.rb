44eb5721d6bace660759a9efa75f5b2a94125bda
wxianfeng <wang.fl1429@gmail.com>
2011-05-21 13:46:41 +0800

    complete colors CRUD

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
   def show_title?
     @show_title
   end
+  
+  def error_messages_for obj
+    html = ''
+    if obj.errors.any?      
+      html << %Q{<div class="errorExplanation">}
+      html << %Q(<ul>)
+      obj.errors.each { |attr,msg|
+        html << %Q(<li> #{msg}</li>)
+      }
+      html << %Q{</ul>}
+      html << %Q{</div>}
+    end
+    raw html
+  end
 end
--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ActiveRecord::Base
   
   validates :name , :presence=>{:message=>"color name cant blank"},:uniqueness=>{:message=>"color name must unique"}
-  validates :value , :presence=>{:message=>"color value cant blank"},:uniqueness=>{:message=>"color value must unique"}
+  validates :value , :presence=>{:message=>"color value cant blank"},:uniqueness=>{:message=>"color value must unique"},:format=>{:with=>/[a-z0-9]/i,:message=>"color value format error"}
   
 end
