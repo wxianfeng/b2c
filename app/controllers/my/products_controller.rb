d7e18025e2357a0298459634822010094effc87a
James wachira <jamomathenge@yahoo.com>
2011-05-26 10:12:48 -0700

    -cleaning up new product form and controller

--- app/controllers/my/products_controller.rb
+++ app/controllers/my/products_controller.rb
@@ -1,16 +1,17 @@
 class My::ProductsController < ApplicationController
+  before_filter :load_product_details, :only => [:new, :create, :edit, :update]
   
   def index
   end
   
   def new
-    @product = Product.new       
+    @product       = Product.new       
   end
   
   def create
     if params[:category_name].blank?
       flash[:error] = "pls select a category"
-      redirect_to :action=>:new  and return 
+      redirect_to :action=> :new  and return 
     end
     category = Category.find_by_name(params[:category_name])
     @product = Product.new(params[:product].merge(:category_id=>category.id))
@@ -28,4 +29,10 @@ class My::ProductsController < ApplicationController
   def show
     @product = Product.find_by_id(params[:id])
   end
+  
+  def load_product_details
+    @colors = Color.all
+    @product_draft = ProductDraft.create
+  end
+  
 end
